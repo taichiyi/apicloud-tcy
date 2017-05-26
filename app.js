b5776b496f207e79fcecee02b472c601f6cbe899
taichiyi <taichiyi@foxmail.com>
2017-05-26 15:13:25 +0800

    每个frame淡入效果

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const tcy = require('./lib/core/tcy.js');
 // tcy.add_frm(frameName, frameTitle);
 // tcy.add_wf(name, title);
 // -------------------------
-// tcy.add_wf('test0', '测试0');
-tcy.createProject('test0');
+tcy.add_wf('test0', '测试0');
+// tcy.createProject('test0');
